71f6bb2544ca3b8448646b3458fa3fe80c2c1272
MX <notanactualemail@gmail.com>
2025-04-16 09:11:56 +0200

    basic task component

---
+++ src/components/task/task.tsx
@@ -0,0 +1,31 @@
+import { Card, IconButton, Input, Stack } from "@mui/material";
+import { CheckBox, CheckBoxOutlineBlank, Delete, Edit, Done, Undo } from "@mui/icons-material";
+
+import { useTypedSelector } from "../../hooks/useTypedSelector";
+import { taskByIdSelector } from "../../store/selectors/tasksSelectors";
+
+export function Task({id}: {id: number}) {
+    const task = useTypedSelector(state => taskByIdSelector(id, state));
+    
+    return (
+        <Card sx={{margin: "5px"}}>
+            <Stack  direction="row" sx={{ justifyContent: "space-evenly" }}>
+                <IconButton sx={{flex: "5", maxHeight: "40px"}} color="primary">
+                    {task.completed ? <CheckBoxOutlineBlank/> : <CheckBox/>}
+                </IconButton>
+
+                <Input disableUnderline multiline sx={{flex: "85"}} value={task.title}>
+                </Input>
+
+                <Stack direction="row" sx={{flex: "10"}}>
+                    <IconButton sx={{maxHeight: "40px"}} color="secondary">
+                        <Edit/>
+                    </IconButton>
+                    <IconButton sx={{maxHeight: "40px"}} color="secondary">
+                        <Delete/>
+                    </IconButton>
+                </Stack>
+            </Stack>
+        </Card>
+    )
+}
--- src/store/selectors/tasksSelectors.ts
+++ src/store/selectors/tasksSelectors.ts
@@ -3,6 +3,10 @@ import { RootState } from "../reducers";
 
 export const allTasksSelector = (state: RootState) => state.tasks.tasks;
 
+export const taskByIdSelector = (id: number, state: RootState) => {
+    return state.tasks.tasks.filter(task => task.id === id)[0];
+}
+
 export const completedTasksSelector = createSelector(
     [allTasksSelector],
     (tasks) => {
