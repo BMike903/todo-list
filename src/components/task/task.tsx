68e003e744d80b8d093d36fc2611abd3304dff2d
MX <notanactualemail@gmail.com>
2025-04-18 13:32:22 +0200

    basic functionality without submiting

--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { Card, IconButton, Input, Stack } from "@mui/material";
 import { CheckBox, CheckBoxOutlineBlank, Edit, Done, Undo } from "@mui/icons-material";
 
@@ -12,6 +14,15 @@ type TaskProps = {
 
 export function Task({id, changeTaskStatus}: TaskProps) {
     const task = useTypedSelector(state => taskByIdSelector(id, state));
+
+    const [title, setTitle] = useState(task.title);
+    const [isEdited, setIsEdited] = useState(false);
+    const toggleEdited = () => setIsEdited(!isEdited);
+
+    const onUndoClick = () => {
+        toggleEdited();
+        setTitle(task.title);
+    }
     
     return (
         <Card sx={{margin: "5px"}}>
@@ -20,13 +31,28 @@ export function Task({id, changeTaskStatus}: TaskProps) {
                     {task.completed ? <CheckBoxOutlineBlank/> : <CheckBox/>}
                 </IconButton>
 
-                <Input disableUnderline multiline sx={{flex: "85"}} value={task.title}>
-                </Input>
+                <Input disableUnderline multiline sx={{flex: "85"}} value={title}
+                    onChange={e => setTitle(e.target.value)} disabled={!isEdited} />
 
                 <Stack direction="row" sx={{flex: "10"}}>
-                    <IconButton sx={{maxHeight: "40px"}} color="secondary">
-                        <Edit/>
-                    </IconButton>
+                    {!isEdited ? 
+                        <IconButton sx={{maxHeight: "40px"}} color="secondary"
+                            onClick={toggleEdited}
+                        >
+                            <Edit/>
+                        </IconButton>
+                    :
+                        <>
+                        <IconButton sx={{maxHeight: "40px"}} color="secondary">
+                            <Done/>
+                        </IconButton>
+                        <IconButton sx={{maxHeight: "40px"}} color="secondary"
+                            onClick={onUndoClick}
+                        >
+                            <Undo/>
+                        </IconButton>
+                        </>
+                    }
                 </Stack>
             </Stack>
         </Card>
