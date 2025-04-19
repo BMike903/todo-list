d07dfe4009d481cd41a81316b98641b84a0a1db3
MX <notanactualemail@gmail.com>
2025-04-19 12:17:22 +0200

    removed unnecessary actions

--- src/store/action-creators/tasks.ts
+++ src/store/action-creators/tasks.ts
@@ -1,5 +1,5 @@
 import { Dispatch } from "@reduxjs/toolkit"
-import { TasksAction, TasksActionTypes, TaskActionTypes, TaskAction, Task, TaskTitleUpdate } from "../../types/tasks"
+import { TasksAction, TasksActionTypes, TaskActionTypes, TaskAction, Task } from "../../types/tasks"
 
 export const fetchTasks = (id: number): any => {
     return async (dispatch: Dispatch<TasksAction>) => {
@@ -16,33 +16,6 @@ export const fetchTasks = (id: number): any => {
         }catch(e){
             console.log(e);
             dispatch({type: TasksActionTypes.FETCH_TASKS_ERROR, 
-                payload: "Error occured while loading tasks"
-            })
-        }
-    }
-}
-
-export const changeTaskStatus = (task: Task): any => {
-    return async (dispatch: Dispatch<TaskAction>) => {
-        try{
-            dispatch({type: TaskActionTypes.CHANGE_TASK_STATUS, payload: task});
-            const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${task.id}`,
-                {
-                    method: "PATCH",
-                    headers:{"Content-Type": "application/json"},
-                    body: JSON.stringify({"completed": !task.completed})
-                } 
-            );
-
-            if(!response.ok){
-                throw new Error(`Failed to update task, status: ${response.status}`);
-            }
-
-            const data = await response.json();
-            dispatch({type: TaskActionTypes.CHANGE_TASK_STATUS_SUCCESS, payload: data})
-        }
-        catch{
-            dispatch({type: TaskActionTypes.CHANGE_TASK_STATUS_ERROR, 
                 payload: "Error occured while loading tasks"
             })
         }
@@ -77,33 +50,6 @@ export const clearDeletingTaskError = (): any => {
     return {type: TasksActionTypes.CLEAR_DELETING_TASK_ERROR};
 }
 
-export const changeTaskTitle = (taskUpdate: TaskTitleUpdate): any => {
-    return async (dispatch: Dispatch<TaskAction>) => {
-        try {
-            dispatch({type: TaskActionTypes.CHANGE_TASK_TITLE, payload: {...taskUpdate}});
-
-            const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${taskUpdate.id}`,
-                {
-                    method: "PATCH",
-                    headers:{"Content-Type": "application/json"},
-                    body: JSON.stringify({"title": taskUpdate.newTitle})
-                } 
-            );
-
-            if(!response.ok){
-                throw new Error(`Failed to update task title, status: ${response.status}`);
-            }
-
-            const data = await response.json();
-            dispatch({type: TaskActionTypes.CHANGE_TASK_TITLE_SUCESS, payload: data})
-        }
-        catch {
-            dispatch({type: TaskActionTypes.CHANGE_TASK_TITLE_ERROR, 
-                payload: "Error occured while updating task"
-            })
-        }
-    }
-}
 
 export const clearUpdateTaskTitleError = () => {
     return {type: TasksActionTypes.CLEAR_UPDATING_TASK_TITLE_ERROR};
